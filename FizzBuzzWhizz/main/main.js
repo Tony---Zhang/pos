f0f781a007d97c6db21f47f1e61b3c4f7fb88400
Zhang Shuai <520zhangshuai@gmail.com>
2017-04-05 21:44:30 +0800

    Add validate function

--- FizzBuzzWhizz/main/main.js
+++ FizzBuzzWhizz/main/main.js
@@ -6,16 +6,30 @@ const whizz = "Whizz";
 const numberSize = 100;
 
 function fizzBuzzWhizz(numbers) {
-  let result;
-  for (let i = 1; i < numberSize; i++) {
-    result = "";
-    if (i.toString().indexOf(numbers[0].toString()) >= 0) {
-      result = fizz;
-    } else {
-      result = rul3And4(numbers[0], numbers[1], numbers[2], i);
+  if (validate(numbers, error => {
+      console.log("Invalidate number: " + error.toString());
+    })) {
+    let result;
+    for (let i = 1; i < numberSize; i++) {
+      result = "";
+      if (i.toString().indexOf(numbers[0].toString()) >= 0) {
+        result = fizz;
+      } else {
+        result = rul3And4(numbers[0], numbers[1], numbers[2], i);
+      }
+      console.log(result);
     }
-    console.log(result);
   }
+}
+
+function validate(numbers, error) {
+  for (let number of numbers) {
+    if (!isPositiveInteger(number)) {
+      error(number);
+      return false;
+    }
+  }
+  return true;
 }
 
 function rul3And4(firstNum, secondNum, thirdNum, num) {
@@ -37,6 +51,11 @@ function rul3And4(firstNum, secondNum, thirdNum, num) {
 
 function isEmpty(str) {
   return (!str || 0 === str.length);
+}
+
+function isPositiveInteger(str) {
+  let n = Math.floor(Number(str));
+  return typeof str === 'number' && n === str && n >= 0;
 }
 
 fizzBuzzWhizz([3, 5, 7]);
