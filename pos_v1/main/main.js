761e3f3b6f8012794863eb56e26c2f6bc54416af
Zhang Shuai <520zhangshuai@gmail.com>
2017-04-11 14:00:23 +0800

    Finish init cart step

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,71 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+function parseItem(input) {
+  let split = input.split('-');
+  return {barcode: split[0], count: parseInt(split[1] || 1)};
+}
+
+function initShoppingCart(input, items) {
+  let shoppingItems = [];
+  input.forEach(inputItem => {
+    let shoppingItem = parseItem(inputItem);
+    let find = shoppingItems.filter(element => {
+      return element.barcode === shoppingItem.barcode;
+    })[0];
+    if (find) {
+      find.count += shoppingItem.count;
+    } else {
+      shoppingItems.push(shoppingItem);
+    }
+  });
+  return shoppingItems.map(element => {
+    let find = items.filter(item => {
+      return item.barcode === element.barcode;
+    })[0];
+    return {item: find, count: element.count};
+  });
+}
+
+let cart = initShoppingCart(['ITEM000003-5', 'ITEM000001', 'ITEM000003-2'], loadAllItems());
+console.log(cart);
+
+function loadAllItems() {
+  return [
+    {
+      barcode: 'ITEM000000',
+      name: '可口可乐',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000001',
+      name: '雪碧',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000002',
+      name: '苹果',
+      unit: '斤',
+      price: 5.50
+    },
+    {
+      barcode: 'ITEM000003',
+      name: '荔枝',
+      unit: '斤',
+      price: 15.00
+    },
+    {
+      barcode: 'ITEM000004',
+      name: '电池',
+      unit: '个',
+      price: 2.00
+    },
+    {
+      barcode: 'ITEM000005',
+      name: '方便面',
+      unit: '袋',
+      price: 4.50
+    }
+  ];
+}
